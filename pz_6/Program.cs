b804bc92caf779932bfd55bff75a30e007ae8542
Максим Бойко <mboyko2005@gmail.com>
2022-10-02 11:05:27 +0500

    pz_6 add

---
+++ pz_6/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int count = 2;
+            int prev = 1;
+            int next = 1;
+            int step = 0;
+
+            while (true)
+            {
+                step = prev;
+                prev = next;
+                next += step;
+                count++;
+                if (next == A)
+                {
+                    Console.WriteLine(count);
+                    break;
+                }
+                else if (next > A)
+                {
+                    Console.WriteLine(-1);
+                    break;
+                }
+            }
+        }
+    }
+}
