628700aaa8ceb867a70967f53a01ac5b62946e5d
Максим Бойко <mboyko2005@gmail.com>
2022-11-10 10:49:12 +0500

    изменено

---
+++ PZ_11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PZ_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] words = input.Split(" .,!?:".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            int count = 0;
+            foreach (string word in words)
+                if (char.ToLower(word[0]) == char.ToLower(word[word.Length - 1]) && word.Length > 1)
+                    count++;
+            Console.WriteLine(count);
+        }
+    }
+}
