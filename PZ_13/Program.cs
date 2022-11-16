009465b11ffe65d328fd05a5b8a80c9876893bf1
Максим Бойко <mboyko2005@gmail.com>
2022-11-16 11:04:29 +0500

    PZ_13 add

---
+++ PZ_13/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PZ_13
+{
+    internal class Program
+    {
+        static string array(string[] m)
+        {
+            int a = m.Length;
+            string max = m[1];
+            for (int i = 0; i < m.Length; i++)
+            {
+                if (m[i].Length > m[1].Length)
+                    max = m[i];
+            }
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк");
+            int b = int.Parse(Console.ReadLine());
+            string[] B = new string[m];
+            for (int i = 0; i < m; i++)
+            {
+                m[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Самая больша строка " + array(B) );
+            Console.ReadLine();
+        }
+    }
+}
