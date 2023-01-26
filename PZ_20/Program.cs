b646dda3eaf31f3f08beb15968e4e9ff92f749a7
Максим Бойко <mboyko2005@gmail.com>
2023-01-26 12:33:13 +0500

    addPZ_20

---
+++ PZ_20/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Pz_20
+{
+	internal class Program
+	{
+		static string maks()
+		{
+			string puk = Console.ReadLine();
+			string antonout = "";
+			for (int i = 0; i < puk.Length; i++)
+			{
+				if (char.IsLower(puk[i]))
+				{
+					antonout += puk[i].ToString().ToUpper();
+
+				}
+				else
+				{
+					antonout += puk[i].ToString().ToLower();
+				}
+
+			}
+			return antonout;
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine(maks());
+		}
+	}
+}
