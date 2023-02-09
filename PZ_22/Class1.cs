dc7a1c91f0f758960246c68fa571bc2f1759b62b
Максим Бойко <mboyko2005@gmail.com>
2023-02-09 09:32:28 +0300

    add_22

---
+++ PZ_22/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PZ_22
+{
+    class Package
+    {
+        public string NameSender { get; set; }
+        public string NameReceiver { get; set; }
+        public string Address { get; set; }
+        public DateTime ReceiptDate { get; set; }
+        public string Type { get; set; }
+        public double Weight { get; set; }
+        private static int totalPackages = 0;
+        private static double totalWeight = 0;
+
+        public Package(string отправитель, string получателя, string адрес, DateTime receiptDate, string тип)
+        {
+            NameSender = отправитель;
+            NameReceiver = получателя;
+            Address = адрес;
+            ReceiptDate = receiptDate;
+            Type = тип;
+
+            totalPackages++;
+            totalWeight += Weight;
+        }
+
+        public static int TotalPackages
+        {
+            get
+            {
+                return totalPackages;
+            }
+        }
+
+        public static double TotalWeight
+        {
+            get
+            {
+                return totalWeight;
+            }
+        }
+
+        public void GetPackageInfo()
+        {
+            Console.WriteLine("Имя отправителя " + NameSender);
+            Console.WriteLine("Получатель " + NameReceiver);
+            Console.WriteLine("Адрес получения: " + Address);
+            Console.WriteLine("Дата: " + ReceiptDate.ToString("dd.MM.yyyy"));
+            Console.WriteLine("Тип отправки: " + Type);
+            Console.WriteLine("Вес поссылки: " + Weight + " kg");
+        }
+    }
+}
--- PZ_22/Program.cs
+++ PZ_22/Program.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 
-namespace PZ_21
+namespace PZ_22
 {
     class Program
     {
         static void Main(string[] args)
         {
             Package package1 = new Package("Anton Schmid", "Farid Safiev", "Chkalova 49", DateTime.Now, "Parcel");
+            package1.Weight = 2;
             package1.GetPackageInfo();
 
             Package package2 = new Package("Kirill Petashvili", "Kirill Abelgans", "Chkalova 52", new DateTime(2022, 12, 31), "Letter");
+            package2.Weight = 0.5;
             package2.GetPackageInfo();
 
             Package package3 = new Package("Pavel Levchenko", "Arina Zadorozhnaya", "Gagarin 42", DateTime.Now, "Express");
+            package3.Weight = 3;
             package3.GetPackageInfo();
 
             Package package4 = new Package("Violette Chevtaykina", "Ilya Tulintsev", "Komsomolskaya 32", new DateTime(2022, 11, 15), "Courier");
+            package4.Weight = 1;
             package4.GetPackageInfo();
         }
     }
