3ce59b837e887ec9aacd295e33e6da318b305f98
Максим Бойко <mboyko2005@gmail.com>
2023-02-09 08:56:27 +0300

    add_21

---
+++ PZ_21/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PZ_21
+{
+    class Package
+    {
+        public string nameDest { get; set; }
+        public string nameSor { get; set; }
+        public string address { get; set; }
+        public DateTime receiptDate { get; set; }
+        public string type { get; set; }
+
+        public Package(string nameDest, string nameSor, string address, DateTime receiptDate, string type)
+        {
+            this.nameDest = nameDest;
+            this.nameSor = nameSor;
+            this.address = address;
+            this.receiptDate = receiptDate;
+            this.type = type;
+        }
+
+        public void GetPackageInfo()
+        {
+            Console.WriteLine("Destination name: " + nameDest);
+            Console.WriteLine("Sender name: " + nameSor);
+            Console.WriteLine("Address: " + address);
+            Console.WriteLine("Receipt date: " + receiptDate);
+            Console.WriteLine("Type: " + type);
+        }
+    }
+}
---
+++ PZ_21/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PZ_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Package package1 = new Package("Антон Шмидт", "Фарид Сафиев", "Чкалова 49", DateTime.Now, "Parcel");
+            package1.GetPackageInfo();
+
+            Package package2 = new Package("Кирилл Петашвили", "Кирилл Абельганс", "Чкалова 52", new DateTime(2022, 12, 31), "Letter");
+            package2.GetPackageInfo();
+
+            Package package3 = new Package("Павел Левченко", "Арина Задорожняя", "Гагарина 42", DateTime.Now, "Express");
+            package3.GetPackageInfo();
+
+            Package package4 = new Package("Виолетта Чевтайкина", "Илья Тулинцев", "Комсомольская 32", new DateTime(2022, 11, 15), "Courier");
+            package4.GetPackageInfo();
+        }
+    }
+}
